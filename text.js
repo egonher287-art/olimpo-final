c6c51200ba7748b17c7f1a8a66ae37c638f27da0
egonher287-art <egonher287@g.educaand.es>
2026-05-15 00:23:38 +0200

    Add files via upload

---
+++ text.js
@@ -0,0 +1,151 @@
+<!DOCTYPE html>
+<html lang="es" id="exe-page-mp61nulq-0rakt3bue">
+<head>
+<meta charset="utf-8">
+<meta name="generator" content="eXeLearning v4.0.0">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="license" type="text/html" href="https://creativecommons.org/licenses/by-sa/4.0/">
+<title>Herramientas para el producto final | Los Misterios del Olimpo</title>
+<link rel="icon" type="image/x-icon" href="../libs/favicon.ico">
+<meta name="description" content="Situación de Aprendizaje dirigida a 1.º de ESO para trabajar la comprensión e interpretación de los mitos griegos mediante actividades cooperativas y herramientas digitales.">
+<script>document.querySelector("html").classList.add("js");</script><script src="../libs/jquery/jquery.min.js"> </script><script src="../libs/common_i18n.js"> </script><script src="../libs/common.js"> </script><script src="../libs/exe_export.js"> </script><script src="../libs/bootstrap/bootstrap.bundle.min.js"> </script><link rel="stylesheet" href="../libs/bootstrap/bootstrap.min.css">
+<script src="../idevices/text/text.js"></script><link rel="stylesheet" href="../idevices/text/text.css">
+<link rel="stylesheet" href="../content/css/base.css"><script src="../theme/style.js"> </script><link rel="stylesheet" href="../theme/style.css">
+</head>
+<body class="exe-export exe-web-site">
+<script>document.body.className+=" js"</script>
+<div class="exe-content exe-export pre-js siteNav-hidden"><a href="#page-mp61nulq-0rakt3bue" id="skipNav">Skip to content</a> <nav id="siteNav">
+<ul>
+<li> <a href="../index.html" class="main-node daddy">Los misterios del Olimpo</a>
+<ul class="other-section">
+<li> <a href="../html/guia-didactica.html" class="daddy">Guía didáctica</a>
+<ul class="other-section">
+<li> <a href="../html/introduccion.html" class="no-ch">Introducción</a>
+</li>
+<li> <a href="../html/contenidos.html" class="no-ch">Contenidos</a>
+</li>
+<li> <a href="../html/actividades.html" class="no-ch">Actividades</a>
+</li>
+<li> <a href="../html/linea-temporal.html" class="no-ch">Línea temporal</a>
+</li>
+<li> <a href="../html/curacion-de-contenidos.html" class="no-ch">Curación de contenidos</a>
+</li>
+<li> <a href="../html/seguridad-digital.html" class="no-ch">Seguridad digital</a>
+</li>
+<li> <a href="../html/evaluacion.html" class="no-ch">Evaluación</a>
+</li>
+</ul>
+</li>
+</ul>
+</li>
+<li> <a href="../html/herramientas-de-comunicacion.html" class="no-ch">Herramientas de comunicación</a>
+</li>
+<li class="active"> <a href="../html/herramientas-para-el-producto-final.html" class="active no-ch">Herramientas para el producto final</a>
+</li>
+<li> <a href="../html/creditos.html" class="no-ch">Créditos</a>
+</li>
+</ul>
+</nav><main id="page-mp61nulq-0rakt3bue" class="page"> 
+<header class="main-header">
+<div class="package-header"><p class="package-title">Los Misterios del Olimpo</p>
+<p class="package-subtitle">Situación de Aprendizaje para 1.º de ESO</p></div>
+<div class="page-header"><h1 class="page-title">Herramientas para el producto final</h1></div>
+</header><div id="page-content-page-mp61nulq-0rakt3bue" class="page-content">
+<article id="block-1778708382758-ee9oyzp4n" class="box">
+<header class="box-head">
+<div class="box-icon exe-icon">
+<img src="../theme/icons/diary_alt.png" alt="">
+</div>
+<h1 class="box-title">Herramientas para el producto final</h1>
+<button class="box-toggle box-toggle-on" title="Toggle content">
+<span>Toggle content</span>
+</button></header>
+<div class="box-content">
+<div id="idevice-1778708382756-y0hwokjzq" class="idevice_node text" data-idevice-path="../idevices/text/" data-idevice-type="text" data-idevice-component-type="json" data-idevice-json-data="{&quot;ideviceId&quot;:&quot;idevice-1778708382756-y0hwokjzq&quot;}">
+<div class="exe-text-template">
+    <p>Para el desarrollo del producto final de esta Situación de Aprendizaje se utilizarán diferentes herramientas digitales que permitirán organizar la información, facilitar el trabajo cooperativo y presentar de forma visual las conclusiones obtenidas durante la investigación de los mitos trabajados en el aula.</p>
+<p>Estas herramientas ayudarán al alumnado a interpretar las historias, construir explicaciones razonadas y elaborar de forma conjunta el expediente del caso mitológico.</p>
+</div>
+</div>
+</div>
+</article><article id="block-1778709208298-ie7xvjxt6" class="box">
+<header class="box-head">
+<div class="box-icon exe-icon">
+<img src="../theme/icons/observe.png" alt="">
+</div>
+<h1 class="box-title">El expediente del caso mitológico</h1>
+<button class="box-toggle box-toggle-on" title="Toggle content">
+<span>Toggle content</span>
+</button></header>
+<div class="box-content">
+<div id="idevice-1778709208296-6jsxdrot4" class="idevice_node text" data-idevice-path="../idevices/text/" data-idevice-type="text" data-idevice-component-type="json" data-idevice-json-data="{&quot;ideviceId&quot;:&quot;idevice-1778709208296-6jsxdrot4&quot;}">
+<div class="exe-text-template">
+    <p>El producto final de esta Situación de Aprendizaje consistirá en la elaboración de un expediente del caso mitológico y su posterior exposición oral.</p>
+<p>Cada grupo trabajará un mito diferente y deberá reconstruir la historia a partir de distintas pistas relacionadas con los personajes, los conflictos y las decisiones presentes en el relato.</p>
+<p>El objetivo no será únicamente comprender el mito, sino también interpretarlo, analizar las acciones de los personajes y construir una explicación razonada a partir de la lectura y del trabajo cooperativo.</p>
+</div>
+</div>
+</div>
+</article><article id="block-1778709283411-x92fu5rro" class="box">
+<header class="box-head">
+<div class="box-icon exe-icon">
+<img src="../theme/icons/interactive.png" alt="">
+</div>
+<h1 class="box-title">Canva y la organización de la información</h1>
+<button class="box-toggle box-toggle-on" title="Toggle content">
+<span>Toggle content</span>
+</button></header>
+<div class="box-content">
+<div id="idevice-1778709283410-npnjs6nn2" class="idevice_node text" data-idevice-path="../idevices/text/" data-idevice-type="text" data-idevice-component-type="json" data-idevice-json-data="{&quot;ideviceId&quot;:&quot;idevice-1778709283410-npnjs6nn2&quot;}">
+<div class="exe-text-template">
+    <p>Para la elaboración del expediente del caso mitológico se utilizará Canva como herramienta principal de diseño y organización visual de la información.</p>
+<p>A través de esta herramienta el alumnado podrá seleccionar las pistas más relevantes, organizar sus conclusiones y presentar de forma clara y creativa su interpretación del mito trabajado.</p>
+<p>Además, Canva permite adaptar el trabajo a diferentes ritmos y niveles de competencia digital, favoreciendo la participación de todo el alumnado.</p>
+</div>
+</div>
+</div>
+</article><article id="block-1778709390462-k2mdeb0c3" class="box">
+<header class="box-head">
+<div class="box-icon exe-icon">
+<img src="../theme/icons/roadmap.png" alt="">
+</div>
+<h1 class="box-title">Google Docs y el trabajo cooperativo</h1>
+<button class="box-toggle box-toggle-on" title="Toggle content">
+<span>Toggle content</span>
+</button></header>
+<div class="box-content">
+<div id="idevice-1778709390460-ymoqbzych" class="idevice_node text" data-idevice-path="../idevices/text/" data-idevice-type="text" data-idevice-component-type="json" data-idevice-json-data="{&quot;ideviceId&quot;:&quot;idevice-1778709390460-ymoqbzych&quot;}">
+<div class="exe-text-template">
+    <p>Durante el proceso de investigación cada grupo trabajará mediante documentos compartidos en Google Docs.</p>
+<p>Esta herramienta permitirá registrar las pistas encontradas, organizar ideas y construir de forma conjunta la interpretación del caso mitológico, facilitando además el seguimiento del trabajo cooperativo por parte del profesorado.</p>
+</div>
+</div>
+</div>
+</article><article id="block-1778709493200-hmv7xnagm" class="box">
+<header class="box-head">
+<div class="box-icon exe-icon">
+<img src="../theme/icons/technology.png" alt="">
+</div>
+<h1 class="box-title">Recursos complementarios</h1>
+<button class="box-toggle box-toggle-on" title="Toggle content">
+<span>Toggle content</span>
+</button></header>
+<div class="box-content">
+<div id="idevice-1778709493198-8b6nrvm3f" class="idevice_node text" data-idevice-path="../idevices/text/" data-idevice-type="text" data-idevice-component-type="json" data-idevice-json-data="{&quot;ideviceId&quot;:&quot;idevice-1778709493198-8b6nrvm3f&quot;}">
+<div class="exe-text-template">
+    <p>Para facilitar el desarrollo del producto final, el alumnado contará también con diferentes recursos visuales y digitales relacionados con la mitología griega, como imágenes, fragmentos literarios, pequeños textos narrativos y materiales de apoyo que ayudarán a interpretar cada mito desde distintas perspectivas.</p>
+</div>
+</div>
+</div>
+</article>
+</div></main><div class="nav-buttons">
+<a href="../html/herramientas-de-comunicacion.html" title="Anterior" class="nav-button nav-button-left"><span>Anterior</span></a>
+<a href="../html/creditos.html" title="Siguiente" class="nav-button nav-button-right"><span>Siguiente</span></a>
+</div>
+<footer id="siteFooter"><div id="siteFooterContent"> <div id="packageLicense" class="cc cc-by-sa"> <p> <span class="license-label">Licencia: </span><a href="https://creativecommons.org/licenses/by-sa/4.0/" class="license">Creative Commons: Reconocimiento - compartir igual 4.0</a></p>
+</div>
+</div></footer>
+</div>
+<p id="made-with-eXe"> <a href="https://exelearning.net/" target="_blank" rel="noopener"> <span>Creado con eXeLearning <span>(nueva ventana)</span></span></a></p>
+</body>
+</html>
